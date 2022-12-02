3b8dbf6624187b78af11779bd00eb611384d4ebe
horaemong <jaeho3836@gmail.com>
2022-12-02 20:11:26 +0900

    로그인 확인

--- index.js
+++ index.js
@@ -48,41 +48,6 @@ app.get('/write', (req, res) => {
   res.sendFile(__dirname + '/write.html')
 });
 
-app.post('/add', (req, res) => {
-  res.send('전송완료');
-  console.log(req.body.title);
-  console.log(req.body.date);
-
-  db.collection('counter').findOne({
-    name: '게시물갯수'
-  }, (err, result) => {
-    total_post = result.totalPost + 1;
-    console.log(total_post);
-    db.collection('post').insertOne({
-      _id: total_post,
-      제목: req.body.title,
-      날짜: req.body.date
-    }, (err, res) => {
-      console.log(total_post + 1);
-      console.log('저장 완료');
-      db.collection('counter').updateOne({
-        name: '게시물갯수'
-      }, {
-        $inc: {
-          totalPost: 1
-        }
-      }, (err, result) => {
-        if (err) {
-          return console.log(err);
-        }
-      });
-    });
-
-
-  });
-
-});
-
 
 app.get('/list', (req, res) => {
 
@@ -242,3 +207,51 @@ passport.deserializeUser(function (아이디, done) {
     });
   })
 });
+
+app.post('/add', (req, res) => {
+
+  res.send('전송완료');
+  console.log(req.body.title);
+  console.log(req.body.date);
+  db.collection('counter').findOne({
+    name: '게시물갯수'
+  }, (err, result) => {
+    total_post = result.totalPost + 1;
+    console.log(total_post);
+    var save_post_data = {
+      _id: total_post,
+      제목: req.body.title,
+      날짜: req.body.date,
+      작성자: req.user._id
+    }
+    db.collection('post').insertOne(save_post_data, (err, res) => {
+      console.log(total_post + 1);
+      console.log('저장 완료');
+      db.collection('counter').updateOne({
+        name: '게시물갯수'
+      }, {
+        $inc: {
+          totalPost: 1
+        }
+      }, (err, result) => {
+        if (err) {
+          return console.log(err);
+        }
+      });
+    });
+
+
+  });
+
+});
+
+
+app.use('/', require('./routes/shop'));
+
+// app.get('/shop/shirts', function (요청, 응답) {
+//   응답.send('셔츠 파는 페이지입니다.');
+// });
+
+// app.get('/shop/pants', function (요청, 응답) {
+//   응답.send('바지 파는 페이지입니다.');
+// });
